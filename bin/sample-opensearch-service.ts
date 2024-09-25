70b2f95e5bf00831a428c596aa9a61ea8ace7a60
Fawad Mazhar <fawad.mazhar@nlacoustics.com>
2024-09-25 13:25:12 +0300

    removing IDs for vpc and public subnets

--- bin/sample-opensearch-service.ts
+++ bin/sample-opensearch-service.ts
@@ -16,8 +16,8 @@ new SampleOpensearchServiceStack(app, 'sample-opensearch-service-dev', {
   osDomainEbsVolumeSize: 20,
   // The number of distinctive opensearch indexes
   osIndexes: ['index-01', 'index-02'],
-  vpcId: 'vpc-00e377b27a8a29ba1',
+  vpcId: 'vpc-xxx',
   subnetIds: {
-    public: ['subnet-015427efb0ddf7e83', 'subnet-0594ebdade15326a3', 'subnet-0f3f1158eedb7dcfd'],
+    public: ['subnet-xxx', 'subnet-yyy', 'subnet-zzz'],
   },
 })
